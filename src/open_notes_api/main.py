05f4d89be4338a6e4c0f9bae9461954f577e2745
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-10-13 18:07:58 -0300

    adiciona esboço das rotas

--- src/open_notes_api/main.py
+++ src/open_notes_api/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from typing import List
+import schema
 
-app = FastAPI()
+app = FastAPI(
+    title="OpenNotes API",
+    description="""API desenvolvida para o **Trabalho 1** da disciplina de sistemas distribuídos."""
+)
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.put("/notes/", response_model=schema.Note)
+async def create_note():
+    return None
+
+
+@app.get("/notes/", response_model=List[schema.Note])
+async def read_notes():
+    return None
+
+
+@app.get("/notes/{note_id}/", response_model=schema.Note)
+async def read_note(note_id: int):
+    return None
+
+
+@app.delete("/notes/{note_id}/")
+async def delete_note(note_id: int):
+    return None
