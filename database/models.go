f9319eb0704b52b87d337c37a471ccfbc24b6a01
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-10-28 15:15:41 -0300

    remove colunas não utilizadas

--- database/models.go
+++ database/models.go
@@ -3,13 +3,11 @@ package database
 import "time"
 
 type Note struct {
-	ID         uint      `gorm:"primaryKey" json:"id"`
-	Title      string    `gorm:"not null" json:"title"`
-	Content    string    `json:"content"`
-	Pinned     bool      `gorm:"default:false" json:"pinned"`
-	Archived   bool      `gorm:"default:false" json:"archived"`
-	CreatedAt  time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt  time.Time `gorm:"autoUpdateTime" json:"updated_at"`
-	PinnedAt   time.Time `json:"pinned_at"`
-	ArchivedAt time.Time `json:"archived_at"`
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Title     string    `gorm:"not null" json:"title"`
+	Content   string    `json:"content"`
+	Pinned    bool      `gorm:"default:false" json:"pinned"`
+	Archived  bool      `gorm:"default:false" json:"archived"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
